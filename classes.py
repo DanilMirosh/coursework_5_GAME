248dea88c499562d118d7edb5d2a1b054f0d8ba1
Danil Mirosh <miroshnichenko.da.vi@mail.ru>
2022-10-31 23:07:41 +0500

    add classes

--- classes.py
+++ classes.py
@@ -1,3 +1,7 @@
+from dataclasses import dataclass
+
+from skills import Skill, FuryPunch, HardShot
+
 
 @dataclass
 class UnitClass:
@@ -10,9 +14,25 @@ class UnitClass:
     skill: Skill
 
 
-WarriorClass =  ... # TODO Инициализируем экземпляр класса UnitClass и присваиваем ему необходимые значения аттрибуотов
+WarriorClass = UnitClass(
+    name="Воин",
+    max_health=100,
+    max_stamina=30,
+    attack=30,
+    stamina=30,
+    armor=10,
+    skill=FuryPunch(),
+) # TODO Инициализируем экземпляр класса UnitClass и присваиваем ему необходимые значения аттрибуотов
 
-ThiefClass = ... # TODO действуем так же как и с войном
+ThiefClass = UnitClass(
+    name="Вор",
+    max_health=50,
+    max_stamina=70,
+    attack=30,
+    stamina=30,
+    armor=5,
+    skill=HardShot(),
+) # TODO действуем так же как и с войном
 
 unit_classes = {
     ThiefClass.name: ThiefClass,
